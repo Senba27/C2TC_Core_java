d913411a1ad72ad1b1971a0215fdca334e03ddb8
senba <senba@DESKTOP-MMQQ3Q1>
2024-08-03 04:19:40 -0700

    Interface

---
+++ Interface/src/com/tnsif/dayeight/interfaces/nestedinterfaces/MyInterface.java
@@ -0,0 +1,8 @@
+package com.tnsif.dayeight.interfaces.nestedinterfaces;
+public interface MyInterface {
+	 void calculateArea();
+	    interface MyInnerInterface {
+	       int  id = 20;
+	       void print();     
+	    }
+}
